6205cc953bb57b2ec27df353e29f826b03ae463c
Asyajs <asyafarafonova69@gmail.com>
2023-05-31 18:27:27 +0300

    first commit

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,26 +1,19 @@
 import React from "react";
 import css from "./ContactList.module.css"
-
+import { ContactItem } from "./ContactItem";
 
 export const ContactList = ({ contactFilter, deleteContact }) => {
-  // const handleClick = (id) => {
-    // deleteContact(id)
-  // } use on onClick
-
   return (
     <ul className={css.contact_list}>
       {contactFilter().map(({ id, name, number }) => {
         return (
-          <li className={css.contact_item} key={id}>
-            {name}:  {number} 
-            <button
-              type="button"
-              className={css.delete_button}
-              onClick={() => deleteContact(id)}
-            >
-              Delete
-            </button>
-          </li>
+          <ContactItem
+            id={id}
+            name={name}
+            number={number}
+            deleteContact={deleteContact}
+            key={id}
+          />
         );
       })}
     </ul>
